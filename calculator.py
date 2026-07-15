45888c4038b569537218b3577b705818267e4100
kailash1306 <kailash.t2024@vitstudent.ac.in>
2026-07-15 17:20:22 +0530

    ver 1.3

--- calculator.py
+++ calculator.py
@@ -1,14 +1,21 @@
-# grades.py
-# A simple student grade calculator
-def calculate_grade(marks):
-    if marks >= 90:
-        return "A"
-    elif marks >= 75:
-        return "B"
-    elif marks >= 60:
-        return "C"
-    else:
-        return "F"
-int marks = int(input("enter your marks"))
-print("Marks:", marks)
-print("Grade:", calculate_grade(marks))
+def add(a,b):
+    return a+b
+def sub(a,b):
+    return a-b
+def mul(a,b):
+    return a*b
+def div(a,b):
+    return a/b
+a=int(input("enter first number :"))
+b=int(input("enter second number :"))
+op = int(input("enter Operation to be done :\n1)Add\n2)Subtract\n3)Multiply\n4)Divide\n: "))
+if(op==1):
+    print(add(a,b))
+elif(op==2):
+    print(sub(a,b))
+elif(op==3):
+    print(mul(a,b))
+elif(op==4):
+    print(div(a,b))
+else:
+    print("Invalid output")
